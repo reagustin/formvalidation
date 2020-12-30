e577c84f6a7713e0e7b8742285a6355021c05dc3
Agustin Re <re_agustin@hotmail.com>
2020-12-29 22:32:50 -0300

    commit 2

--- lib/src/bloc/provider.dart
+++ lib/src/bloc/provider.dart
@@ -10,7 +10,6 @@ class Provider extends InheritedWidget {
   bool updateShouldNotify(InheritedWidget oldWidget) => true;
 
   static LoginBloc of(BuildContext context) {
-    return (context.inheritFromWidgetOfExactType(Provider) as Provider)
-        .loginBloc;
+    return context.dependOnInheritedWidgetOfExactType<Provider>().loginBloc;
   }
 }
