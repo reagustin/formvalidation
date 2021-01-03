5e2c3059aa59119fe7b89861170de895dde298b3
Agustin Re <re_agustin@hotmail.com>
2021-01-03 01:12:26 -0300

    v5

--- lib/src/bloc/login_bloc.dart
+++ lib/src/bloc/login_bloc.dart
@@ -20,6 +20,10 @@ class LoginBloc with Validators {
   Function(String) get changeEmail => _emailController.sink.add;
   Function(String) get changePassword => _passwordController.sink.add;
 
+  // Obtener el ultimo valor ingresado a los strings
+  String get email => _emailController.value;
+  String get password => _passwordController.value;
+
   dispose() {
     _emailController?.close();
     _passwordController?.close();
--- lib/src/pages/login_page.dart
+++ lib/src/pages/login_page.dart
@@ -15,21 +15,31 @@ class LoginPage extends StatelessWidget {
 
   Widget _crearBoton(LoginBloc bloc) {
     return StreamBuilder(
-        stream: bloc.formValidStream,
-        builder: (BuildContext context, AsyncSnapshot snapshot) {
-          return RaisedButton(
-            child: Container(
-              padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
-              child: Text('Ingresar'),
-            ),
-            shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(5.0)),
-            elevation: 0.0,
-            color: Colors.deepPurple,
-            textColor: Colors.white,
-            onPressed: snapshot.hasData ? () {} : null,
-          );
-        });
+      stream: bloc.formValidStream,
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        return RaisedButton(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 80.0, vertical: 15.0),
+            child: Text('Ingresar'),
+          ),
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(5.0)),
+          elevation: 0.0,
+          color: Colors.deepPurple,
+          textColor: Colors.white,
+          onPressed: snapshot.hasData ? () => _login(bloc, context) : null,
+        );
+      },
+    );
+  }
+
+  _login(LoginBloc bloc, BuildContext context) {
+    print('================================');
+    print('Email: ${bloc.email}');
+    print('Password: ${bloc.password}');
+    print('================================');
+
+    Navigator.pushReplacementNamed(context, 'home');
   }
 
   Widget _crearFondo(BuildContext context) {
